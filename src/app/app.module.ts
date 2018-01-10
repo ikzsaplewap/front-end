c0beb444470631b884c7ed09fdde3350ea7b5e69
Pawel Paszki <pawelpaszki@gmail.com>
2018-01-10 21:29:07 +0000

    partial images page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { ImagesComponent } from './images/images.component';
+import { DropdownModule } from 'angular-custom-dropdown';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImagesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
