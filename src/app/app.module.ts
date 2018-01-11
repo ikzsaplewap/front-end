9f0d5ee2f1c70317967bf2cd77ecf9f8dba9c8f0
Pawel Paszki <pawelpaszki@gmail.com>
2018-01-11 16:28:10 +0000

    search bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { ImagesComponent } from './images/images.component';
 import { FreshnessIndicatorComponent } from './images/freshness-indicator.component';
 import { FreshnessDefinitionComponent } from './images/freshness-definition.component';
 import { DropdownModule } from 'angular-custom-dropdown';
+import { ContainersComponent } from './containers/containers.component';
+import { SearchImagesComponent } from './images/search-images.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImagesComponent,
     FreshnessIndicatorComponent,
-    FreshnessDefinitionComponent
+    FreshnessDefinitionComponent,
+    ContainersComponent,
+    SearchImagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/images/search-images.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-images',
+  templateUrl: './search-images.component.html',
+  styleUrls: ['./search-images.component.css']
+})
+export class SearchImagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
